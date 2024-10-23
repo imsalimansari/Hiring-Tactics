a299360d748ac38ca22f388b4c822ff582b6b647
Salim Ansari <salimansari7644@gmail.com>
2024-10-23 22:32:10 +0530

    First Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/shared/Navbar";
 import Home from "./components/Home";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
+import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
 
 const appRouter = createBrowserRouter([
   {
@@ -17,6 +19,14 @@ const appRouter = createBrowserRouter([
     path: "/Signup",
     element: <Signup />,
   },
+  {
+    path:"/jobs",
+    element: <Jobs/>
+  },
+  {
+    path:"/browse",
+    element: <Browse/>
+  }
 ]);
 function App() {
   return (
