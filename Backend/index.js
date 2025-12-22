f33918a898a742b446aa9aa6d82407e51777b473
imsalimansari <salimansari7644@gmail.com>
2025-12-22 13:40:04 +0530

    Initial commit: Frontend and Backend setup

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://hiring-tactics-1.onrender.com",
     credentials:true
 }
 app.use(cors(corsOptions));
