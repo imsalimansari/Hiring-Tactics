c61f06d77e483a4c9bd0e9e61967db49648342b5
Salim Ansari <150717361+imsalimansari@users.noreply.github.com>
2025-12-28 23:41:21 +0530

    Update project title and add live project link

--- Frontend/src/components/user/Team.jsx
+++ Frontend/src/components/user/Team.jsx
@@ -11,7 +11,7 @@ const Team = () => {
         <div className="flex flex-wrap justify-center gap-8">
           {/* Circle Item 1 */}
           <div className="flex flex-col items-center space-y-4">
-            <div className="w-[210px] h-[210px] rounded-full overflow-hidden flex items-center justify-center hover:rounded-md">
+            <div className="w-[210px] h-[210px] rounded-full overflow-hidden flex items-center justify-center">
               <img src="/img1.jpg" alt="Team Member 1" className="w-full h-full object-cover" />
             </div>
             <h2 className="text-xl font-bold text-gray-800">Salim Ansari</h2>
@@ -19,7 +19,7 @@ const Team = () => {
 
           {/* Circle Item 2 */}
           <div className="flex flex-col items-center space-y-4">
-            <div className="w-[210px] h-[210px] rounded-full overflow-hidden flex items-center justify-center hover:rounded-md">
+            <div className="w-[210px] h-[210px] rounded-full overflow-hidden flex items-center justify-center">
               <img src="/img3.jpg" alt="Team Member 2" className="w-full h-full object-cover" />
             </div>
             <h2 className="text-xl font-bold text-gray-800">Riya Singh</h2>
